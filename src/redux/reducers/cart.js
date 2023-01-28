95483eb97d93bb7b58a5011f797f6d4996f4895d
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-28 13:01:41 +0700

    Add cart.js

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,39 @@
+import { actionStrings } from "../actions/actionStrings";
+
+const initalState = {
+  checkout: {},
+  orderSummaryOfCheckout: {},
+  addToCart: [],
+  orderSummaryOfAddToCart: [],
+};
+
+const cartReducer = (prevState = initalState, { type, payload }) => {
+  switch (type) {
+    case actionStrings.addCart:
+      console.log("Reducer Payload: ", payload);
+      return {
+        ...prevState,
+        addToCart: payload.data,
+      };
+    case actionStrings.checkoutProduct:
+      return {
+        ...prevState,
+        checkout: payload.data,
+      };
+    case actionStrings.orderSummaryOfCheckout:
+      return {
+        ...prevState,
+        orderSummaryOfCheckout: payload.data,
+      };
+    case actionStrings.orderSummaryOfAddToCart:
+      return {
+        ...prevState,
+        orderSummaryOfAddToCart: payload.data,
+      };
+
+    default:
+      return prevState;
+  }
+};
+
+export default cartReducer;
