c2cb818c255657d9a7f100e64943de5be54e17c4
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-28 13:18:57 +0700

    Add profile.js

---
+++ src/redux/actions/profile.js
@@ -0,0 +1,110 @@
+import { ActionType } from "redux-promise-middleware";
+import {
+  editProfile,
+  getProfileContact,
+  getProfileDetail,
+} from "../../utils/api/profile";
+import { actionStrings } from "./actionStrings";
+
+const { Pending, Rejected, Fulfilled } = ActionType;
+
+// TODO: Edit Profile actions
+const editProfilePending = () => ({
+  type: actionStrings.editProfile.concat("-", Pending),
+});
+
+const editProfileRejected = (error) => ({
+  type: actionStrings.editProfile.concat("-", Rejected),
+  payload: { error },
+});
+
+const editProfileFulfilled = (data) => ({
+  type: actionStrings.editProfile.concat("-", Fulfilled),
+  payload: { data },
+});
+
+// TODO: Get Profile Contact actions
+const getProfileContactPending = () => ({
+  type: actionStrings.getProfileContact.concat("-", Pending),
+});
+
+const getProfileContactRejected = (error) => ({
+  type: actionStrings.getProfileContact.concat("-", Rejected),
+  payload: { error },
+});
+
+const getProfileContactFulfilled = (data) => ({
+  type: actionStrings.getProfileContact.concat("-", Fulfilled),
+  payload: { data },
+});
+
+// TODO: Get Profile Detail actions
+const getProfileDetailPending = () => ({
+  type: actionStrings.getProfileDetail.concat("-", Pending),
+});
+
+const getProfileDetailRejected = (error) => ({
+  type: actionStrings.getProfileDetail.concat("-", Rejected),
+  payload: { error },
+});
+
+const getProfileDetailFulfilled = (data) => ({
+  type: actionStrings.getProfileDetail.concat("-", Fulfilled),
+  payload: { data },
+});
+
+// TODO: Edit Profile Thunk
+const editProfileThunk = (body, token, cbFulfilled, cbError) => {
+  return async (dispatch) => {
+    try {
+      dispatch(editProfilePending());
+      const result = await editProfile(body, token);
+      // console.log(result.data);
+      dispatch(editProfileFulfilled(result.data));
+      typeof cbFulfilled === "function" && cbFulfilled();
+    } catch (error) {
+      dispatch(editProfileRejected(error));
+      typeof cbError === "function" && cbError();
+    }
+  };
+};
+
+// TODO: Get Profile Thunk
+const getProfileContactThunk = (token, cbFulfilled, cbError) => {
+  return async (dispatch) => {
+    try {
+      dispatch(getProfileContactPending());
+      const result = await getProfileContact(token);
+      // console.log(result.data);
+      dispatch(getProfileContactFulfilled(result.data));
+      typeof cbFulfilled === "function" && cbFulfilled();
+    } catch (error) {
+      dispatch(getProfileContactRejected(error));
+      typeof cbError === "function" && cbError();
+    }
+  };
+};
+
+// TODO: Get Profile Detail Thunk
+const getProfileDetailThunk = (token, cbFulfilled, cbDenied) => {
+  return async (dispatch) => {
+    try {
+      dispatch(getProfileDetailPending());
+      const result = await getProfileDetail(token);
+      // console.log(result);
+      dispatch(getProfileDetailFulfilled(result.data));
+      typeof cbFulfilled === "function" && cbFulfilled();
+    } catch (error) {
+      dispatch(getProfileDetailRejected(error));
+      typeof cbDenied === "function" && cbDenied();
+    }
+  };
+};
+
+const ProfilesAction = {
+  editProfileThunk,
+  getProfileContactThunk,
+  getProfileDetailThunk,
+};
+
+export default ProfilesAction;
---
+++ src/redux/reducers/profile.js
@@ -0,0 +1,100 @@
+import { ActionType } from "redux-promise-middleware";
+import { actionStrings } from "../actions/actionStrings";
+
+let initialState;
+
+initialState = {
+  editProfileResult:[],
+  resultProfileContact: [],
+  resultProfileDetail: [],
+  isLoading: false,
+  isError: false,
+  isFulfilled: false,
+  err: null,
+};
+
+const profileReducer = (prevState = initialState, { payload, type }) => {
+  const { Pending, Rejected, Fulfilled } = ActionType;
+  const { editProfile, getProfileContact, getProfileDetail } = actionStrings;
+  switch (type) {
+    case editProfile.concat("-", Pending):
+      return {
+        ...prevState,
+        isLoading: true,
+        isError: false,
+        isFulfilled: false,
+      };
+    case editProfile.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: false,
+        isError: false,
+        isFulfilled: true,
+        editProfileResult: payload.data.result,
+      };
+    case editProfile.concat("-", Rejected):
+      return {
+        ...prevState,
+        isLoading: false,
+        isError: true,
+        isFulfilled: false,
+        err: payload.error.result?.data.result.msg,
+      };
+
+    case getProfileContact.concat("-", Pending):
+      return {
+        ...prevState,
+        isLoading: true,
+        isError: false,
+        isFulfilled: false,
+      };
+    case getProfileContact.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: false,
+        isError: false,
+        isFulfilled: true,
+        resultProfileContact: payload.data.result,
+      };
+    case getProfileContact.concat("-", Rejected):
+      return {
+        ...prevState,
+        isLoading: false,
+        isError: true,
+        isFulfilled: false,
+        err: payload.error.message,
+      };
+
+    case getProfileDetail.concat("-", Pending):
+      return {
+        ...prevState,
+        isLoading: true,
+        isError: false,
+        isFulfilled: false,
+      };
+    case getProfileDetail.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: false,
+        isError: false,
+        isFulfilled: true,
+        resultProfileDetail: payload.data.result,
+      };
+    case getProfileDetail.concat("-", Rejected):
+      return {
+        ...prevState,
+        isLoading: false,
+        isError: true,
+        isFulfilled: false,
+        err: payload.error.message,
+      };
+
+    default:
+      return prevState;
+  }
+};
+
+export default profileReducer;
