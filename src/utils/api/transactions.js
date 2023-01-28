896fa93a1783645e7655647fe9b075b80e191b15
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-28 13:06:34 +0700

    Updated transactions.js

--- src/utils/api/transactions.js
+++ src/utils/api/transactions.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const BASE_URL = `${process.env.REACT_APP_BACKEND_HOST}api/v1`;
+const BASE_URL = `${process.env.REACT_APP_BACKEND_HOST}api/v1/`;
 
 const config = (accessToken) => {
   return {
@@ -11,26 +11,31 @@ const config = (accessToken) => {
 };
 
 const getTransactions = (accessToken) =>
-  Axios.get(`${BASE_URL}/transactions`, config(accessToken));
+  Axios.get(`${BASE_URL}transactions`, config(accessToken));
 
 const createTransaction = (body, accessToken) =>
-  Axios.post(`${BASE_URL}/transactions`, body, config(accessToken));
+  Axios.post(`${BASE_URL}transactions/create`, body, config(accessToken));
+
+const editTransaction = (id, body, accessToken) =>
+  Axios.post(`${BASE_URL}transactions/edit${id}`, body, config(accessToken));
 
 const updateStatusTransaction = (id, body, accessToken) =>
-  Axios.patch(`${BASE_URL}/transactions/${id}`, body, config(accessToken));
-
-const deleteTransaction = (id, accessToken) =>
-  Axios.delete(
+  Axios.patch(
     `${BASE_URL}/transactions/status/update/${id}`,
+    body,
     config(accessToken)
   );
 
+const deleteTransaction = (id, accessToken) =>
+  Axios.delete(`${BASE_URL}transactions/delete/${id}`, config(accessToken));
+
 const getHistoryTransaction = (accessToken) =>
-  Axios.get(`${BASE_URL}/transactions/history`, config(accessToken));
+  Axios.get(`${BASE_URL}transactions/history`, config(accessToken));
 
 export {
   getTransactions,
   createTransaction,
+  editTransaction,
   updateStatusTransaction,
   deleteTransaction,
   getHistoryTransaction,
