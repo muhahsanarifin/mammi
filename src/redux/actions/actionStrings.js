a2c23bea99672dc356466529fd5e4ad82186bf0d
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-28 13:00:53 +0700

    Updated action strings

--- src/redux/actions/actionStrings.js
+++ src/redux/actions/actionStrings.js
@@ -1,4 +1,31 @@
 export const actionStrings = {
-  // Product Action String
+  // Product
   getProducts: "PRODUCTS_GET",
+
+  // Profile
+  editProfile: "PROFILE_EDIT",
+  getProfileContact: "PROFILE_CONTACT_GET",
+  getProfileDetail: "PROFILE_DETAIL_GET",
+
+  // Counters
+  // counterUp: "COUNTER_UP",
+  // counterDown: "COUNTER_DOWN",
+  // counterReset: "COUNTER_RESET"
+
+  // Cart
+  addCart: "ADD_CART",
+  // Checkout
+  checkoutProduct: "CHECKOUT_PRODUCT",
+
+  // Order Summary
+  orderSummaryOfCheckout: "ORDER_SUMMARY_CHECKOUT",
+  orderSummaryOfAddToCart: "ORDER_SUMMARY_ADDTOCART",
+
+  // Transactions
+  getTransactions: "TRANSACTIONS_GET",
+  createTransaction: "TRANSACTION_CREATE",
+  editTransaction: "TRANSACTION_EDIT",
+  deleteTransaction: "TRANSACTION_DELELTE",
+  updateStatusTransaction: "STATUS_TRANSACTION_UPDATE",
+  getHistoryTransaction: "HISTORY_TRANSACTION_GET",
 };
