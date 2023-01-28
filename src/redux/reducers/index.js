0fe8f1e31f2cd9c75892b568efa50f3a4c547ab4
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-28 13:03:48 +0700

    Updated index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import productsReducer from "./products";
+import profileReducer from "./profile";
+import cartReducer from "./cart";
+import transactionReducer from "./transactions";
 
 export default combineReducers({
   products: productsReducer,
+  profiles: profileReducer,
+  cart: cartReducer,
+  transactions: transactionReducer,
 });
