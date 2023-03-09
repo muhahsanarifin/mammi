bf82685744af6575f60f3c5dfd77a2a3791ddfda
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-10 01:55:20 +0700

    Updated component

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 import styles from "../styles/Skeleton.module.css";
 
-const ProductCardSkeleton = ({ products }) => { // Developer does not use it temporarily
+const ProductCardSkeleton = () => {
   return (
     <>
-      {products.map((product) => (
-        <span className={`col my-3 ${styles.product}`} key={product.id}>
-          <div className={styles["product__image"]}></div>
-          <div className={styles["product__decs"]}>
-            <div className={styles["product__name"]}></div>
-            <div className={styles["product__price"]}></div>
-          </div>
-        </span>
-      ))}
+      <span className={`col my-3 ${styles.product}`}>
+        <div className={styles["product__image"]}></div>
+        <div className={styles["product__decs"]}>
+          <div className={styles["product__name"]}></div>
+          <div className={styles["product__price"]}></div>
+        </div>
+      </span>
     </>
   );
 };
